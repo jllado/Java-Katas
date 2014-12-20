b01afb4a344163fa123b8152c307f434ee069361
Juan Llado <jllado@gmail.com>
2014-12-20 20:43:37 +0100

    Removed title attribute from Book because YAGNI.

--- src/main/java/katas/potter/Book.java
+++ src/main/java/katas/potter/Book.java
@@ -1,11 +1,5 @@
 package katas.potter;
 
 public enum Book {
-    BOOK1("Book 1"), BOOK2("Book 2"), BOOK3("Book 3"), BOOK4("Book 4"), BOOK5("Book 5");
-    
-    private final String tittle;
-
-    Book(String title) {
-        this.tittle = title;
-    }
+    BOOK1(), BOOK2(), BOOK3(), BOOK4(), BOOK5();
 }
