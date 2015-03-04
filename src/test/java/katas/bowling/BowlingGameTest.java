92ea898f8a790d9af34e8e2164021c54112eadbd
Juan Llado <jllado@gmail.com>
2015-03-04 21:19:38 +0100

    Red test: get_score_given_one_roll

--- src/test/java/katas/bowling/BowlingGameTest.java
+++ src/test/java/katas/bowling/BowlingGameTest.java
@@ -13,6 +13,7 @@ public class BowlingGameTest {
     @Test
     public void get_score_given_one_roll() throws Exception {
         assertThat(scoreOf("1"), is(1));
+        assertThat(scoreOf("2"), is(2));
     }
 
     private int scoreOf(String roll) {
