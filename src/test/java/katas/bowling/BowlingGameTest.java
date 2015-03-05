f54c0afdaee58410b1d37075747c41ed89cda22c
Juan Llado <jllado@gmail.com>
2015-03-05 22:24:07 +0100

    Green test: get_score_given_game_with_one_spare_frame

--- src/test/java/katas/bowling/BowlingGameTest.java
+++ src/test/java/katas/bowling/BowlingGameTest.java
@@ -39,6 +39,7 @@ public class BowlingGameTest {
         assertThat(scoreOf(new String[]{"1", "/", "2", "1"}), is(15));
         assertThat(scoreOf(new String[]{"1", "2", "2", "/", "3"}), is(16));
         assertThat(scoreOf(new String[]{"1", "/", "2", "1", "3", "4"}), is(22));
+        assertThat(scoreOf(new String[]{"1", "-", "2", "/", "3", "4"}), is(21));
     }
 
     @Test
