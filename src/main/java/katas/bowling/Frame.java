56046a8bc231a4e6ef2880b5a0b4f23e18deabcf
Juan Llado <jllado@gmail.com>
2015-03-06 03:25:13 +0100

    Refactor: unused code removed

--- src/main/java/katas/bowling/Frame.java
+++ src/main/java/katas/bowling/Frame.java
@@ -2,40 +2,28 @@ package katas.bowling;
 
 public class Frame {
 
-    private Roll[] tries;
+    private Roll[] rolls;
 
     public Frame(Roll[] rolls) {
-        this.tries = rolls;
+        this.rolls = rolls;
     }
 
     private boolean isSpare() {
-        return tries[1].isSpare();
+        return rolls[1].isSpare();
     }
 
     private boolean isStrike() {
-        return tries[0].isStrike();
-    }
-
-    private int spareScore() {
-        return 10 + getExtraRoll(tries, 1).score();
-    }
-
-    private int strikeScore() {
-        return 10 + getExtraRoll(tries, 0).score() + getExtraRoll(tries, 1).score();
-    }
-
-    private Roll getExtraRoll(Roll[] frame, int extraRoll) {
-        return frame[1 + extraRoll];
+        return rolls[0].isStrike();
     }
 
     public int score() {
         if (isStrike()) {
-            return 10 + getExtraRoll(tries, 0).score() + getExtraRoll(tries, 1).score();
+            return 10 + rolls[1].score() + rolls[2].score();
         }
         if (isSpare()) {
-            return 10 + getExtraRoll(tries, 1).score();
+            return 10 + rolls[2].score();
         }
 
-        return tries[(0)].score() + tries[1].score();
+        return rolls[(0)].score() + rolls[1].score();
     }
 }
