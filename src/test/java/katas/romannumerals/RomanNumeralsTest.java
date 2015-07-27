b1b8b6e58305edcbd9be21c8005cce77bf1a8593
Juan Llado <jllado@gmail.com>
2015-07-27 23:12:27 +0200

    Red test: should_return_I_given_1

---
+++ src/test/java/katas/romannumerals/RomanNumeralsTest.java
@@ -0,0 +1,30 @@
+package katas.romannumerals;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by jllado on 27/07/15.
+ */
+/*
+1 - I
+2 - II
+5 - V
+5 - IV
+6 - VI
+9 - IX
+10 - X
+ */
+public class RomanNumeralsTest {
+    @Test
+    public void should_return_I_given_1() throws Exception {
+
+        assertThat(getRomanNumeral(1), is("I"));
+    }
+
+    private String getRomanNumeral(int i) {
+        return "";
+    }
+}
