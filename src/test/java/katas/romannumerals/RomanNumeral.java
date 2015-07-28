67bff9e608f445e6b24c168d6bee2dc48716017e
Juan Llado <jllado@gmail.com>
2015-07-28 23:24:28 +0200

    Green test: should_return_MCMXC_given_1990

--- src/test/java/katas/romannumerals/RomanNumeral.java
+++ src/test/java/katas/romannumerals/RomanNumeral.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class RomanNumeral {
     public enum Value {
-        X(10), V(5), I(1);
+        M(1000), C(100), X(10), V(5), I(1);
 
         private final int digit;
 
@@ -35,11 +35,20 @@ public class RomanNumeral {
                 romanNumeral += value.toString();
                 remainDigit -= value.getDigit();
             }
-            if (remainDigit > 0 && remainDigit == value.getDigit() - 1) {
-                romanNumeral += Value.I + value.toString();
-                remainDigit -= (value.getDigit() - 1);
+            if (remainDigit > 0 && remainDigit / (value.getDigit() - getPreviousValue(value).getDigit()) > 0) {
+                romanNumeral += getPreviousValue(value) + value.toString();
+                remainDigit -= (value.getDigit() - getPreviousValue(value).getDigit());
             }
         }
         return romanNumeral.toString();
+    }
+
+    private Value getPreviousValue(Value value) {
+        for (Value previousValue : Value.getValues()) {
+            if (previousValue.getDigit() < value.getDigit() && !String.valueOf(previousValue.getDigit()).contains("5")) {
+                return previousValue;
+            }
+        }
+        return Value.I;
     }
 }
